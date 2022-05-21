1e96ed188f2fe88c3ad77143f3867b972bc524d5
zn-brk <borek@student.agh.edu.pl>
2022-05-22 00:22:31 +0200

    work on the matrix

--- scripts/sample_sol.py
+++ scripts/sample_sol.py
@@ -2,18 +2,17 @@ import random
 # sample case = {'skills': list of skills names, 'candidates': list of candidates names,
 # 'positions': list of positions ids, 'candidates_skills': [lists of skills for every candidate],
 # 'position_skills': [list od skills needed for every position]}
-def gen_sample_solution(sample_case):
-    n = len(sample_case['positions'])
-    m = len(sample_case['candidates'])
+def get_solution(case, sol_mn):
+    n = len(case['positions'])
+    m = len(case['candidates'])
     solution = dict()
-    solution_matrix = [[0 for _ in range (m)] for _ in range (n)]
-    for position in sample_case['positions']:
+    for position in case['positions']:
         solution[position] = []
-    for candidate_idx, candidate in enumerate(sample_case['candidates']):
+    for candidate_idx, candidate in enumerate(case['candidates']):
         idx = random.randint(0,n-1)
-        solution[sample_case['positions'][idx]].append(candidate)
-        solution_matrix[idx][candidate_idx] = 1
-    return (solution, solution_matrix)
+        solution[case['positions'][idx]].append(candidate)
+        sol_mn[idx][candidate_idx] = 1
+    return solution
 
 def gen_sample_sol_mn(m: int, n: int):
     solution_matrix = [[0 for _ in range (m)] for _ in range (n)]
